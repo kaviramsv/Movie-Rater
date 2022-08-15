317833f36cf018fcbeffa2aa2dbfb0a82ee6caf9
kaviramsv <kavitasvenkitachalam@gmail.com>
2022-08-15 19:08:20 -0400

    Add logic to add review

--- client/src/components/AddReview.jsx
+++ client/src/components/AddReview.jsx
@@ -1,15 +1,41 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { Navigate, useLocation, useNavigate, useParams } from 'react-router-dom';
+import MovieFinder from '../apis/MovieFinder';
 
 const AddReview = () => {
+
+  const { id } = useParams();
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  console.log(location);
 
   const [name, setName] = useState("");
   const [rating, setRating] = useState("");
   const [review, setReview] = useState("");
+
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const result = await MovieFinder.post(`/${id}/reviews`, {
+        name,
+        review,
+        rating,
+      });
+      console.log("add reviews", result.rows);
+      window.location.reload();
+    } catch (err) {
+      console.log((err));
+    }
+  }
+
+
   return (
     <div className="mb-2" >
       <form>
         <div className="form-row ">
-          <div className="form-group col-8 ">
+          <div className="form-group col-4 ">
             <label htmlFor='name'>Name</label>
             <input id="name"
               placeholder="name"
@@ -18,19 +44,20 @@ const AddReview = () => {
               onChange={ev => setName(ev.target.value)}
               type="text"
               className="form-control" />
-          </div>
-          <div className="form-group col-8 mt-3  ">
-            <label htmlFor='rating'>Rating</label>
-            <select id='rating' className="form-select"
-              value={rating}
-              onChange={ev => setRating(ev.target.value)}>
-              <option disabled>Rating</option>
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-            </select>
+
+            <div className="form-group col-2 mt-3  ">
+              <label htmlFor='rating'>Rating</label>
+              <select id='rating' className="form-select"
+                value={rating}
+                onChange={ev => setRating(ev.target.value)}>
+                <option disabled>Rating</option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+              </select>
+            </div>
           </div>
         </div>
         <div className="form-group mt-3 col-8">
@@ -39,7 +66,7 @@ const AddReview = () => {
             value={review}
             onChange={ev => setReview(ev.target.value)}>
           </textarea>
-          <button className="btn btn-primary mt-5  row-15">
+          <button className="btn btn-primary mt-5  row-15" onClick={submitHandler}>
             Submit
           </button>
         </div>
